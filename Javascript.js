08943d5652020cb47b0d765eb56e77ad82b5fed8
Husniddin6939 <atoullayevhusniddin@gmail.com>
2024-03-29 13:54:04 +0300

    finished

---
+++ Javascript.js
@@ -0,0 +1,84 @@
+//14-masala
+
+// let son =+prompt("sonni kiriting");
+// let yuz =(son%1000-son%100)/100;
+// let on =(son%100-son%10)/10;
+// let bir =(son%10);
+
+// alert((bir+"") + (yuz+"") + (on+""));
+
+
+//15-masala
+
+// let son =+prompt("sonni kiriting");
+// let yuz =(son%1000-son%100)/100;
+// let on =(son%100-son%10)/10;
+// let bir =(son%10);
+
+// alert((on+"") + (yuz+"") + (bir+""));
+
+
+//16-masala
+
+// let son =+prompt("sonni kiriting");
+// let yuz =(son%1000-son%100)/100;
+// let on =(son%100-son%10)/10;
+// let bir =(son%10);
+
+// alert((yuz+"") + (bir+"") + (on+""));
+
+//17-masala
+
+// let son =+prompt("Sonni kiriting");
+// let yuzliklar =(son%1000-son%100)/100;
+
+// alert(yuzliklar+"");
+
+
+//18-masala
+
+// let son =+prompt("Sonni kiriting");
+// let minglar =(son-son%1000)/1000
+
+// alert(minglar+"");
+
+//19-masala
+
+// let sekunt=+prompt("Sekundni kiriting");
+// let minut= (sekunt/60);
+
+
+// alert(minut);
+
+
+//20-masala
+
+// let sekunt=+prompt("Sekundni kiriting");
+// let minut=(sekunt/60);
+// let soat= (minut/60);
+
+// alert(soat);
+
+//21-masala
+
+// let sekunt=+prompt("Sekundni kiriting");
+// let minut=(sekunt%60);
+// let qoldiq=(minut - sekunt)
+
+// alert((minut+"minut") + (qoldiq+"sekunt"))
+
+
+
+//22-masala
+
+// let sekund = +prompt("Necha sekund otdi?");
+// let soat =  (sekund/60);
+// alert("Soat " + Math.trunc(sekund / 3600) + " sekund " + sekund % 60);
+
+
+
+//23-masala
+
+// let sekund = +prompt("Necha sekund otdi?");
+// let soat =  (sekund/60);
+// alert(Math.trunc(sekund / 3600) + " Soat " + Math.trunc(sekund / 60) + " minut " + sekund % 60 + " sekund " );
